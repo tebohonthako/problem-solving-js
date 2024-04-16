f47a90b6ed1207630e88e381cbfa7367902e71d2
tebohonthako <tjaynthako@gmail.com>
2024-04-16 15:17:50 +0200

    notes by mdu

---
+++ mdu.js
@@ -0,0 +1,28 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// let i = 0;
+// while(i < 1000){
+// let name1 = prompt("person  "+i+" enter your name...");
+// let mark1 = parseInt(prompt("enter 1st mark..."));
+// let mark2 = parseInt(prompt("enter 2nd mark..."));
+// let mark3 = parseInt(prompt("enter 3rd mark..."));
+
+// var status = ''; //empty string
+// let sum = mark1 + mark2 + mark3;
+// let average = parseInt(sum / 3);
+
+// if(average < 50 ){
+//    status = "That was an epic fail!"; // store the message inside an empty string 
+// } else if( average >= 50 && average < 75){
+//     status = "You passed! Good job! ✅";
+// } else if(average >= 75 && average <= 100) {
+//    status = "Excellent work! You got a distinction! 🌟💯"
+// }else {
+//     console.log("error")
+// }
+
+
+// console.log(`Your final mark is ${average}, ${status}`);
+//  i++;
+// }
